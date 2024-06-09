6bfee044a79564b468efee1766a956ccbf715fd1
João Pedro da Cruz Nobre <jpedrocruz78@gmail.com>
2024-06-09 14:55:29 -0400

    update responsive navbar and footer date

---
+++ src/clinet/app/date.js
@@ -0,0 +1,4 @@
+const copyrightDiv = document.querySelector(".copyright");
+const currentYear = new Date().getFullYear();
+
+copyrightDiv.innerHTML= `© ${currentYear} Copyright <br> João Pedro da Cruz Nobre, inc`;
