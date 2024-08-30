2869b6eebff506695e0c7028a4d03546bc824726
João Pedro <jpedrocruz78@gmail.com>
2024-08-30 10:39:03 -0400

    add components to home

--- src/client/app/components/footer/footer.js
+++ src/client/app/components/footer/footer.js
@@ -2,7 +2,8 @@ import tmplFooter from './footer.ejs'
 
 export default async () => {
     console.log('loadingFooter')
-    const strFooter = tmplFooter();
+    const currentYear = new Date().getFullYear();
+    const strFooter = tmplFooter({ currentYear });
     // console.log(strFooter);
     document.getElementById('app').insertAdjacentHTML("afterend", strFooter);
 }
